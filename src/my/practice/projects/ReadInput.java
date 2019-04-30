34f9077e2acc63fa5701c7dddf2f33deb17b836c
nandini <nandini9590@gmail.com>
2019-04-30 18:09:43 -0400

    Making code changes

---
+++ src/my/practice/projects/ReadInput.java
@@ -0,0 +1,24 @@
+package my.practice.projects;
+
+import java.util.Scanner;
+
+/**
+ * Created by Nandini on 4/30/2019.
+ */
+public class ReadInput {
+
+    private static void main(String[] args) {
+
+            // Creates a reader instance which takes
+            // input from standard input - keyboard
+            Scanner reader = new Scanner(System.in);
+            System.out.print("Enter a number: ");
+
+            // nextInt() reads the next integer from the keyboard
+            int number = reader.nextInt();
+
+            // println() prints the following line to the output screen
+            System.out.println("You entered: " + number);
+        }
+    }
+
