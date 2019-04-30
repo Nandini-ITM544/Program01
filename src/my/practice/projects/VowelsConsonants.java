1ce9aecadbb7bce3c14e2c797bef68a17c6e8b60
nandini <nandini9590@gmail.com>
2019-04-30 19:01:45 -0400

    Vowels and conso

---
+++ src/my/practice/projects/VowelsConsonants.java
@@ -0,0 +1,25 @@
+package my.practice.projects;
+
+/**
+ * Created by Nandini on 4/30/2019.
+ */
+
+/* fix it for upper case */
+public class VowelsConsonants {
+
+    private static void checkVowels ( char ch)
+    {
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+        {
+            System.out.println("it is a vowel" + ch);
+        }
+        else
+            System.out.println("it is not a  vowel" + ch);
+    }
+
+    public static void main (String[] args)
+    {
+checkVowels('A');
+    }
+
+}
